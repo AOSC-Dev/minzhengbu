3d67fd45c4e7befb5cd5401adbe8c75cd5fc12b2
eatradish <sakiiily@aosc.io>
2023-12-13 16:49:49 +0800

    feat: add telegram id storage

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 use axum::{
     extract::Query,
-    response::Redirect,
-    routing::{get, post},
-    Json, Router,
+    response::{Html, Redirect},
+    routing::get,
+    Router,
 };
-use once_cell::sync::Lazy;
-use serde::Deserialize;
+use dashmap::DashMap;
+use once_cell::sync::{Lazy, OnceCell};
+use rand::{distributions::Alphanumeric, Rng};
+use redis::{aio::MultiplexedConnection, AsyncCommands};
+use serde::{Deserialize, Serialize};
 use url::Url;
 
 #[derive(Deserialize, Debug)]
@@ -13,7 +16,7 @@ struct CallbackLoginArgs {
     code: String,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Serialize, Debug)]
 struct CallbackSecondLoginArgs {
     access_token: String,
     expires_in: i64,
@@ -28,12 +31,22 @@ struct ErrMessage {
     err: String,
 }
 
+#[derive(Deserialize, Debug)]
+struct TelegramInfo {
+    telegram_id: String,
+    rid: String,
+}
+
+static TEMP_MAP: Lazy<DashMap<String, CallbackSecondLoginArgs>> = Lazy::new(DashMap::new);
+
 static CLIENT_ID: Lazy<String> =
     Lazy::new(|| std::env::var("GITHUB_CLIENT_ID").expect("GITHUB_CLIENT_ID is not set"));
 static CLIENT_SECRET: Lazy<String> =
     Lazy::new(|| std::env::var("GITHUB_CLIENT_SECRET").expect("GITHUB_CLIENT_SECRET is not set"));
 static REDIRECT_URL: Lazy<String> =
     Lazy::new(|| std::env::var("REDIRECT_URL").expect("REDIRECT_URL is not set"));
+
+static DB_CONN: OnceCell<MultiplexedConnection> = OnceCell::new();
 
 #[tokio::main]
 async fn main() {
@@ -46,20 +59,52 @@ async fn main() {
     let _ = &*CLIENT_SECRET;
     let _ = &*REDIRECT_URL;
 
+    let client =
+        redis::Client::open("redis://127.0.0.1/").expect("Failed to connect redis database");
+
+    let connect = client
+        .get_multiplexed_tokio_connection()
+        .await
+        .expect("Failed to get multiplexed connection");
+
+    DB_CONN.get_or_init(|| connect);
+
     // build our application with a route
     let app = Router::new()
         // `GET /` goes to `root`
         .route("/login", get(login))
         .route("/", get(root))
-        .route("/error", get(error));
+        .route("/error", get(error))
+        .route("/login_from_telegram", get(login_from_telegram));
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
 
+async fn login_from_telegram(Query(payload): Query<TelegramInfo>) -> String {
+    let TelegramInfo { telegram_id, rid } = payload;
+
+    let access_info = TEMP_MAP.get(&rid);
+
+    match access_info {
+        Some(access_info) => {
+            let mut conn = DB_CONN.get().unwrap().to_owned();
+            let s = match serde_json::to_string(access_info.value()) {
+                Ok(s) => s,
+                Err(e) => return format!("Failed to serialize access info: {e}"),
+            };
+            match conn.set(telegram_id, s).await {
+                Ok(()) => "Success".to_owned(),
+                Err(e) => format!("Got Error: {e}"),
+            }
+        }
+        None => "Failed to get access info, You need to re-verify.".to_owned(),
+    }
+}
+
 async fn login(Query(payload): Query<CallbackLoginArgs>) -> Redirect {
-    let code = payload.code;
+    let CallbackLoginArgs { code } = payload;
 
     let mut url = Url::parse("https://github.com/login/oauth/access_token").unwrap();
     url.query_pairs_mut().extend_pairs(&[
@@ -79,23 +124,33 @@ async fn login(Query(payload): Query<CallbackLoginArgs>) -> Redirect {
     match resp {
         Ok(resp) => match resp.text().await {
             Ok(s) => Redirect::permanent(&format!("/?{s}")),
-            Err(e) => return Redirect::permanent(&format!("/error?err={e}")),
+            Err(e) => Redirect::permanent(&format!("/error?err={e}")),
         },
-        Err(e) => return Redirect::permanent(&format!("/error?err={e}")),
+        Err(e) => Redirect::permanent(&format!("/error?err={e}")),
     }
 }
 
-async fn root(Query(payload): Query<CallbackSecondLoginArgs>) -> &'static str {
-    let CallbackSecondLoginArgs {
-        access_token,
-        expires_in,
-        refresh_token,
-        refresh_token_expires_in,
-        scope,
-        token_type,
-    } = payload;
+async fn root(Query(payload): Query<CallbackSecondLoginArgs>) -> Html<String> {
+    // dbg!(payload);
+    let insert_temp_map = tokio::spawn(async {
+        let rng = rand::thread_rng();
+        let s: String = rng
+            .sample_iter(&Alphanumeric)
+            .take(20)
+            .map(char::from)
+            .collect();
 
-    "Hello"
+        TEMP_MAP.insert(s.clone(), payload);
+
+        s
+    });
+
+    match insert_temp_map.await {
+        Ok(s) => Html::from(format!(
+            "<a href=\"https://t.me/aosc_buildit_bot?start={s}\">Hit me!</a>"
+        )),
+        Err(e) => Html::from(format!("Got error: {e}")),
+    }
 }
 
 async fn error(Query(payload): Query<ErrMessage>) -> String {
